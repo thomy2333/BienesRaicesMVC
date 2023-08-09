8c7d42a846326af7ebabf20d8450b106283383a2
thomy2333 <thomyrb30@gmail.com>
2023-08-09 15:53:05 -0300

    MVC

--- controllers/propiedadController.php
+++ controllers/propiedadController.php
@@ -2,16 +2,68 @@
 
 namespace Controllers;
 use MVC\Router;
+use Model\Propiedad;
+use Model\Vendedor;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PropiedadController {
     public static function index(Router $router){
+
+        $propiedades = Propiedad::all();
+        $resultado = $_GET['resultado'] ?? null;
+
        $router->render('propiedades/admin', [
-        
-       ]);
+            'propiedades' => $propiedades,
+            'resultado' => $resultado
+        ]);
     }
 
-    public static function crear(){
-        echo "crear";
+    public static function crear(Router $router){
+
+        $propiedad = new Propiedad;
+        $vendedores = Vendedor::all();
+        $errores = Propiedad::getErrores();
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            //crea una nueva instancia
+            $propiedad = new Propiedad($_POST['propiedad']);
+        
+            //SUBIDA DE ARCHIVOS 
+            //generar un nombre unico
+            $nombreImagen = md5( uniqid( rand(), true)) .  ".jpg";
+
+            //setar al imagen
+            //realizar un resize a la imagen con itervecion 
+            if($_FILES['propiedad']['tmp_name']['imagen']){
+                $imagen = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
+                $propiedad->setImagen($nombreImagen);
+            }
+        
+
+            //validar
+            $errores =  $propiedad->validar();      
+
+            //revisar el arreglo de errores este vacio
+            if(empty($errores)){
+                //crear la carpeta para subir imagenes
+                if(!is_dir(CARPETA_IMAGENES)){
+                mkdir(CARPETA_IMAGENES);
+                }
+
+                //guarda la imagen en el servidor
+                $imagen->save(CARPETA_IMAGENES . $nombreImagen);
+
+                //guardar en la base de datos
+                $propiedad->crear();           
+            }
+
+        }
+
+        $router->render('propiedades/crear', [
+            'propiedad' => $propiedad,
+            'vendedores' => $vendedores,
+            'errores' => $errores,
+        ]);
     }
 
     public static function actualizar(){
--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -1,1 +1,48 @@
-<?php echo $mensaje; ?>
+<main class="contenedor seccion">
+        <h1>Administrado de Bienes Raices</h1>  
+
+        <?php 
+            if ($resultado) {
+                $mensaje = mostrarNotificacion(intval($resultado));
+                if ($mensaje) { ?>
+                   <p class="alerta exito"><?php echo s($mensaje) ?></p>
+             <?php }    
+            }           
+        ?>
+
+
+        <a href="/propiedades/crear" class="boton boton-verde">Nueva Propiedad</a>
+        <a href="/admin/vendedores/crear.php" class="boton boton-amarillo">Nueva Vendedor</a>
+
+        <h2>Propiedades</h2>
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody> <!-- mostrar resultados -->
+                <?php foreach($propiedades as $propiedad): ?>
+                <tr>
+                    <td><?php echo $propiedad->id; ?></td>
+                    <td><?php echo $propiedad->titulo; ?></td>
+                    <td><img src="/imagenes/<?php echo $propiedad->imagen; ?>" class="imagen-tabla"></td>
+                    <td>$ <?php echo $propiedad->precio; ?></td>
+                    <td>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
+                            <input type="hidden" name="tipo" value="propiedad">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+</main>
